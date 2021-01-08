2efd010330117b0daa6ccf18039829c4e314ae5f
calreynoldsitext <68069344+calreynoldsitext@users.noreply.github.com>
2021-01-08 13:47:05 -0500

    Update overflow-wrap.cs

--- overflow-wrap.cs
+++ overflow-wrap.cs
@@ -2,5 +2,5 @@ public virtual void ConvertHTMLtoPDF() {
     ConverterProperties properties = new ConverterProperties();
     FontProvider fontProvider = new DefaultFontProvider(true, true, true);
     properties.SetFontProvider(fontProvider);
-    HtmlConverter.ConvertToPdf(new FileInputStream("HTML/input_html.html"), new FileOutputStream("output_file.pdf"), properties);
+    HtmlConverter.ConvertToPdf(new FileStream("HTML/input_html.html", FileMode.Open, FileAccess.Read), new FileStream("HTML/input_html.html", FileMode.Create), properties);
 }
