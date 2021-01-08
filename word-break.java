c55504ed95dae8f97a3b7432b5af200708c54e74
calreynoldsitext <68069344+calreynoldsitext@users.noreply.github.com>
2021-01-08 13:41:39 -0500

    Update word-break.java

--- word-break.java
+++ word-break.java
@@ -5,5 +5,5 @@ public void convertHTMLtoPDF() throws IOException {
     ConverterProperties properties = new ConverterProperties();
     FontProvider fontProvider = new DefaultFontProvider(true, true, true);
     properties.setFontProvider(fontProvider);
-    HtmlConverter.convertToPdf(new FileInputStream("HTML/target-counter.html"), new FileOutputStream("output_file.pdf"), properties);
+    HtmlConverter.convertToPdf(new FileInputStream("HTML/input_html.html"), new FileOutputStream("output_file.pdf"), properties);
 }
