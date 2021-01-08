6ddeadffee53e70a0e8c9c1859a02330f4d50779
calreynoldsitext <68069344+calreynoldsitext@users.noreply.github.com>
2021-01-08 13:41:56 -0500

    Update word-break.cs

--- word-break.cs
+++ word-break.cs
@@ -5,5 +5,5 @@ public virtual void ConvertHTMLtoPDF() {
     ConverterProperties properties = new ConverterProperties();
     FontProvider fontProvider = new DefaultFontProvider(true, true, true);
     properties.SetFontProvider(fontProvider);
-    HtmlConverter.ConvertToPdf(new FileInputStream("HTML/target-counter.html"), new FileOutputStream("output_file.pdf"), properties);
+    HtmlConverter.ConvertToPdf(new FileInputStream("HTML/input_html.html"), new FileOutputStream("output_file.pdf"), properties);
 }
