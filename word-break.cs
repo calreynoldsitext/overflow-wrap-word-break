e70aec35ae08f6aee1ef9eae35afa13adf29ac15
calreynoldsitext <68069344+calreynoldsitext@users.noreply.github.com>
2021-01-08 13:33:54 -0500

    Create word-break.cs

---
+++ word-break.cs
@@ -0,0 +1,9 @@
+public virtual void ConvertHTMLtoPDF() throws IOException {
+    FontProvider fontProvider = new FontProvider();
+    FontProgram fontProgram = FontProgramFactory.CreateFont("NotoSansJP-Medium.otf");
+    fontProvider.AddFont(fontProgram);
+    ConverterProperties properties = new ConverterProperties();
+    FontProvider fontProvider = new DefaultFontProvider(true, true, true);
+    properties.SetFontProvider(fontProvider);
+    HtmlConverter.ConvertToPdf(new FileInputStream("HTML/target-counter.html"), new FileOutputStream("output_file.pdf"), properties);
+}
